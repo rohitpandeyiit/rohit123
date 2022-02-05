20248b91918c0a74ef863288db5179ffd5fe1ade
rohitpandeyiit <rohitpandeyiit@gmail.com>
2022-02-06 03:43:10 +0530

    Insertion of element in array.

---
+++ DSA/src/com/Array/A1InsertionOfElementInArray.java
@@ -0,0 +1,18 @@
+package com.Array;
+
+import java.util.Arrays;
+
+public class A1InsertionOfElementInArray {
+
+	public static void main(String[] args) {
+		int[] arr;
+		arr = new int[4];
+		arr[0] = 1;
+		arr[1] = 2;
+		arr[2] = 3;
+		arr[3] = 4;
+		System.out.println(Arrays.toString(arr));//[1, 2, 3, 4]
+		
+	}
+
+}
